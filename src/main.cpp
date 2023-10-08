58dec510528b220b8ac810efd8e325dc1bd7ea95
jxu720 <126126593+jxu720@users.noreply.github.com>
2023-10-08 12:09:16 -0500

    add throttle driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+
+#include "inverter_driver.h"
+#include "throttle_driver.h"
 #include "virtualTimer.h"
 
 #define SERIAL_DEBUG
@@ -10,20 +13,54 @@ ESPCAN can_bus{10, gpio_num_t::GPIO_NUM_22, gpio_num_t::GPIO_NUM_21};
 // Structure for handling timers
 VirtualTimerGroup read_timer;
 
+// Instantiate throttle
+Throttle throttle{can_bus};
+
+// Instantiate inverter
+Inverter inverter(can_bus);
+
+enum class BMSState
+{
+    kShutdown = 0,
+    kPrecharge = 1,
+    kActive = 2,
+    kCharging = 3,
+    kFault = 4
+};
+
+enum class BMSCommand
+{
+    NoAction = 0,
+    PrechargeAndCloseContactors = 1,
+    Shutdown = 2
+};
+
+enum state
+{
+    OFF,
+    N,
+    DRIVE,
+    FDRIVE
+};
+
+// CAN Signals
+
+void changeState()
+{
+    // Write code here
+}
+
+void processState()
+{
+    // Write code here
+}
+
 void setup()
 {
-#ifdef SERIAL_DEBUG
-    // Initialize serial output
-    Serial.begin(9600);  // Baud rate (Can transfer max of 9600 bits/second)
-#endif
-
-    // Initialize can bus
-    can_bus.Initialize(ICAN::BaudRate::kBaud1M);
+    // Write code here
 }
 
 void loop()
 {
-    delay(0);
-    read_timer.Tick(millis());
-    can_bus.Tick();
+    // Write code here
 }
